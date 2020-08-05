0730f5e54627ed2d8650b563fc7bc891aa1c8162
Anu-BakingCode <34658371+Anu-BakingCode@users.noreply.github.com>
2020-08-05 19:01:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "${var.aws_region}"
   #shared_credentials_file = "/home/ubuntu/.aws/credentials"
-  access_key = "AKIA3XFSVHB2OLAS4HHY"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
